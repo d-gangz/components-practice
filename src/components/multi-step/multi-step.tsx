c68f1df30a9cb30108682da8d9c8c7afef88add6
limgangrui <104761504+d-gangz@users.noreply.github.com>
2024-11-19 20:10:25 +0800

    multi-step component done

--- src/components/multi-step/multi-step.tsx
+++ src/components/multi-step/multi-step.tsx
@@ -6,6 +6,8 @@ import useMeasure from "react-use-measure";
 
 export default function MultiStepComponent() {
   const [currentStep, setCurrentStep] = useState(0);
+  // This is used to determine the direction of the animation and it can either be 1 or -1
+  const [direction, setDirection] = useState<number>(1);
   const [ref, bounds] = useMeasure();
 
   const content = useMemo(() => {
@@ -64,6 +66,7 @@ export default function MultiStepComponent() {
   return (
     // Use MotionConfig to set the default transition for all the components inside it
     <MotionConfig transition={{ duration: 0.5, type: "spring", bounce: 0 }}>
+      {direction}
       <motion.div
         animate={{ height: bounds.height }}
         className="relative mx-auto my-24 w-[550px] overflow-hidden rounded-xl shadow-[0_0_0_1px_rgba(0,0,0,0.08),0px_2px_2px_rgba(0,0,0,0.04),0px_8px_8px_-8px_rgba(0,0,0,0.04)]"
@@ -71,15 +74,20 @@ export default function MultiStepComponent() {
         <div ref={ref} className="p-6">
           {/* AnimatePresence is used to animate the content of the current step. We use 110% instead of 100% to account for the padding
         It is important to set the key so that we let react know that it has been updated. Need the poplayout so that it ensure that the exiting element wont take space in the DOM. */}
-          <AnimatePresence mode="popLayout" initial={false}>
+          <AnimatePresence mode="popLayout" initial={false} custom={direction}>
+            {/* By adding the custom prop in the animatePresence, we ensure that the exiting component uses the latest direction value. This will solve that weird bug where the exiting component was still using the previous direction value. */}
             <motion.div
               key={currentStep}
-              initial={{ x: "110%", opacity: 0 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ x: "-110%", opacity: 0 }}
+              // We use the variants to define the initial, animate and exit states so we dont havet o keep repeating them. We also need to somehow pass the direction props to the variants. We can use the custom props to pass variables to the variants.
+              variants={variants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              custom={direction}
               // transition={{ duration: 0.5, type: "spring", bounce: 0 }}
             >
               {content}
+              {direction}
             </motion.div>
           </AnimatePresence>
 
@@ -91,10 +99,11 @@ export default function MultiStepComponent() {
           >
             <button
               className="h-8 w-20 rounded-full text-sm font-medium text-[#63635d] shadow-[0_0_0_1px_rgba(0,0,0,0.08),0px_2px_2px_rgba(0,0,0,0.04),0px_8px_8px_-8px_rgba(0,0,0,0.04)] hover:text-black disabled:cursor-not-allowed disabled:opacity-50"
-              disabled={currentStep === 0}
+              disabled={currentStep === 0} // disable button when on first step
               onClick={() => {
-                if (currentStep === 0) return;
-                setCurrentStep((prev) => prev - 1);
+                if (currentStep === 0) return; // extra safety check
+                setDirection(-1); // set animation direction to left.
+                setCurrentStep((prev) => prev - 1); // move to previous step
               }}
             >
               Back
@@ -102,13 +111,15 @@ export default function MultiStepComponent() {
 
             <button
               className="relative h-8 w-24 overflow-hidden rounded-full bg-gradient-to-b from-[#1994ff] to-[#157cff] text-sm font-semibold text-white shadow-[0px_0px_1px_1px_rgba(255,255,255,0.08)_inset,0px_1px_1.5px_0px_rgba(0,0,0,0.32),0px_0px_0px_0.5px_#1a94ff] disabled:cursor-not-allowed disabled:opacity-50"
-              disabled={currentStep === 2}
+              disabled={currentStep === 2} // disable button when on last step
               onClick={() => {
                 if (currentStep === 2) {
-                  setCurrentStep(0);
+                  setCurrentStep(0); // reset to first step if it is the last step
+                  setDirection(-1);
                   return;
                 }
-                setCurrentStep((prev) => prev + 1);
+                setDirection(1); // set animation direction to right.
+                setCurrentStep((prev) => prev + 1); // move to next step
               }}
             >
               Continue
@@ -119,3 +130,14 @@ export default function MultiStepComponent() {
     </MotionConfig>
   );
 }
+
+// We use the custom props to pass the direction variable to the variants. The custom prop is specified in the motion.div with the variants prop. So they are used in conjunction with the variants.
+const variants = {
+  initial: (direction: number) => ({ x: `${direction * 110}%`, opacity: 0 }),
+  animate: { opacity: 1, x: 0 },
+  exit: (direction: number) => ({ x: `${direction * -110}%`, opacity: 0 }),
+};
+
+// So currently we have a problem where the leaving component is still using the previous direction value. To fix this, we can use animate presence.
+
+// here is the definition: custom: any. When a component is removed, there's no longer a chance to update its props. So if a component's exit prop is defined as a dynamic variant and you want to pass a new custom prop, you can do so via AnimatePresence. This will ensure all leaving components animate using the latest data.
