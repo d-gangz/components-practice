f7535967adb9e949be8ecb12ea1f13f6de5d7749
limgangrui <104761504+d-gangz@users.noreply.github.com>
2024-11-20 08:55:36 +0800

    trash animation completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-// import LoginButton from "@/components/button/login-button";
-// import ChatInput from "@/components/chat-input";
-// import AnimatedModals from "@/components/animated-modals/animated-modals";
-// import DynamicHeight from "@/components/dynamic-height/dynamic-height";
-//import FeedbackComponent from "@/components/feedback-popover/feedback-popover";
-// import RectangleToggle from "@/components/rectangle-toggle/rectangle-toggle";
-import MultiStepComponent from "@/components/multi-step/multi-step";
+import { TrashAnimation } from "@/components/trash/trash-animation";
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 min-h-screen items-center justify-center">
-      {/* <ChatInput /> */}
-      {/* <LoginButton /> */}
-      {/* <RectangleToggle /> */}
-      {/* <AnimatedModals /> */}
-      {/* <DynamicHeight /> */}
-      {/* <FeedbackComponent /> */}
-      <MultiStepComponent />
+      <TrashAnimation />
     </div>
   );
 }
